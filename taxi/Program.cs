fbbf387d9b1324385be2ec4dabfe6d7a7298e516
Jan woźniak <nicedark@outlook.com>
2022-02-04 22:57:55 +0100

    Poprawienie błedów

--- taxi/Program.cs
+++ taxi/Program.cs
@@ -2,11 +2,19 @@
 
 namespace taxi
 {
-    class Program
+
+}
+class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+      
+            Console.WriteLine("Wybierz opcję: ");
+            Console.WriteLine("1 =>Lista wszystkich dzielnic i taksówek");
+            Console.WriteLine("2 =>ZAMÓW TAKSÓWKĘ");
+            Console.WriteLine("3 =>ZAKOŃCZ PROGRAM");
+            Console.WriteLine("WYBIERZ 1,2 LUB 3 ");
+        
+        
     }
 }
--- taxi/TaxiSerivice.cs
+++ taxi/TaxiSerivice.cs
@@ -6,27 +6,33 @@ using System.Threading.Tasks;
 
 namespace taxi
 {
-    public class TaxiSerivice()
+    public class TaxiSerivice
     {
-        CreateDistricts();
-    CreateCabs();
+        public TaxiSerivice()
+        {
+            CreateDistricts();
+            CreateCabs();
+        }
+        public List<District> Districts { get; set; } = new List<District>();
+        public List<Cab> Cabs { get; set; } = new List<Cab>();
+        private void CreateDistricts()
+        {
+            Districts.Add(new District(1, "Retkinia", -2));
+            Districts.Add(new District(2, "Łódź Kaliska", -1));
+            Districts.Add(new District(3, "Śródmieście", 0));
+            Districts.Add(new District(4, "Widzew", 1));
+            Districts.Add(new District(5, "Janów", 3));
+        }
+        private void CreateCabs()
+        {
+            Cabs.Add(new Cab(1, "Ford Mondeo", Districts[0]));
+            Cabs.Add(new Cab(2, "Dacia Logan", Districts[1]));
+            Cabs.Add(new Cab(3, "Toyota Avensis", Districts[2]));
+            Cabs.Add(new Cab(4, "Mercedes E220	", Districts[3]));
+            Cabs.Add(new Cab(5, "Huindai Lantra", Districts[4]));
+        }
+    }
 }
-public List<District> Districts { get; set; } = new List<District>();
-public List<Cab> Cabs { get; set; } = new List<Cab>();
-private void CreateCabs()
-{
-    Cabs.Add(new Cab(1, "Ford Mondeo", District[0]);
-    Cabs.Add(new Cab(2, "Dacia Logan", District[1]);
-    Cabs.Add(new Cab(3, "Toyota Avensis", District[2]);
-    Cabs.Add(new Cab(4, "Mercedes E220	", District[3]);
-    Cabs.Add(new Cab(5, "Huindai Lantra", District[4]);
-}
-private void CreateDistricts
-{
-    Districts.Add(new District(1, "Retkinia", -2));
-Districts.Add(new District(2, "Łódź Kaliska", -1));
-Districts.Add(new District(3 "Śródmieście", 0));
-Districts.Add(new District(4, "Widzew", 1));
-Districts.Add(new District(5, "Janów", 3));
-}
-}
+    
+
+
