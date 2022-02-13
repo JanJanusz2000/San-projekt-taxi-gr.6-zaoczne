aefd24f19930b900ab6485155197c56e91b77c9a
JanJanusz2000 <janjanuszpiotr@gmail.com>
2022-02-13 18:03:32 +0100

    add Function ShowAllDistrictAndTaxi ()

--- taxi/Program.cs
+++ taxi/Program.cs
@@ -7,14 +7,17 @@ namespace taxi
 class Program
     {
         static void Main(string[] args)
-        {
-      
+        { 
             Console.WriteLine("Wybierz opcję: ");
             Console.WriteLine("1 =>Lista wszystkich dzielnic i taksówek");
             Console.WriteLine("2 =>ZAMÓW TAKSÓWKĘ");
             Console.WriteLine("3 =>ZAKOŃCZ PROGRAM");
             Console.WriteLine("WYBIERZ 1,2 LUB 3 ");
-        
-        
+            Console.ReadLine();
+
+        static bool CzyDobryKlawisz(string klawisz)
+        {
+            return klawisz == "1" || klawisz == "2" || klawisz == "3" ;
+        }
     }
 }
