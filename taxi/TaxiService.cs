452d50a653caa42ce4b27ef81775d04595434aa4
JanJanusz2000 <janjanuszpiotr@gmail.com>
2022-02-19 13:59:43 +0100

    Zatrzymanie się na problemie z wywołaniem funkcji CalculateArrivalTime

--- taxi/TaxiService.cs
+++ taxi/TaxiService.cs
@@ -15,6 +15,7 @@ namespace taxi
         }
         public List<District> Districts { get; set; } = new List<District>();
         public List<Cab> Cabs { get; set; } = new List<Cab>();
+        public List<District> CustomerDistrict { get; set; } = new List<District>();
         private void CreateDistricts()
         {
             Districts.Add(new District(1, "Retkinia", -2));
@@ -30,6 +31,93 @@ namespace taxi
             Cabs.Add(new Cab(3, "Toyota Avensis", Districts[2]));
             Cabs.Add(new Cab(4, "Mercedes E220	", Districts[3]));
             Cabs.Add(new Cab(5, "Huindai Lantra", Districts[4]));
+        }
+        public void Menu()
+        {
+            Console.WriteLine("Wybierz opcję: ");
+            Console.WriteLine("1 =>Lista wszystkich dzielnic i taksówek");
+            Console.WriteLine("2 =>ZAMÓW TAKSÓWKĘ");
+            Console.WriteLine("3 =>ZAKOŃCZ PROGRAM");
+            Console.WriteLine("WYBIERZ 1,2 LUB 3 ");
+            string key = Console.ReadLine();
+            if (key == "1") { ShowAllDistrictAndTaxi(); }
+            else if (key == "2") { Order(); }
+            else if (key == "3") { return; }
+            else
+            {
+                Console.Clear();
+                Menu();
+            }
+        }
+        static bool IsItaGoodDistrict(string Ordervalue)
+        {
+            return Ordervalue == "1" || Ordervalue == "2" || Ordervalue == "3" || Ordervalue == "4" || Ordervalue == "5";
+        }
+        public void ShowAllDistrictAndTaxi()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Lista DZIELNIC");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("NUMER | NAZWA | ILOŚĆ TAKSÓWEK");
+            foreach (District item in Districts)
+            {
+
+                int CabsInDistrict = 0;
+                foreach (Cab Car in Cabs)
+                {
+                    if (Car.CurrentDistrict.Name == item.Name)
+                        CabsInDistrict++;
+                }
+
+                Console.WriteLine($"{item.Number} | {item.Name} | {CabsInDistrict}");
+            }
+            Console.WriteLine("SAMOCHÓD | STATUS | AKTUALNA LOKALIZACJA");
+            foreach (Cab item in Cabs)
+            {
+                Console.WriteLine($"{item.Car} | {item.Status} | {item.CurrentDistrict.Name}");
+            }
+            Menu();
+        }
+        public void Order()
+        {
+            Console.WriteLine("PROSZĘ PODAĆ NUMER DZIELNICY DO KTÓREJ CHCESZ WEZWAĆ TAKSÓWKĘ:");
+            string districtvalue = Console.ReadLine();
+            bool goodkey = IsItaGoodDistrict(districtvalue);
+            while (!goodkey)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("NIEPRAWIDŁOWY NUMER DZIELNICY");
+                Console.ForegroundColor = ConsoleColor.White;
+                Order();
+            }
+            int OrderValue = int.Parse(districtvalue);
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine($"ZLECENIE REALIZUJE: { }");
+            //  Console.WriteLine($"CZAS DOJAZDU: { }");
+            Cab.CalculateArrivalTime(Districts[OrderValue]);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Lista DZIELNIC");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("NUMER | NAZWA | ILOŚĆ TAKSÓWEK");
+            foreach (District item in Districts)
+            {
+                int CabsInDistrict = 0;
+                foreach (Cab Car in Cabs)
+                {
+                    if (Car.CurrentDistrict.Name == item.Name) { CabsInDistrict++; }
+
+                }
+                Console.WriteLine($"{item.Number} | {item.Name} | {CabsInDistrict}");
+
+            }
+            foreach (Cab item in Cabs)
+            {
+                Console.WriteLine($"{item.Car} | {item.Status} | {item.CurrentDistrict.Name} | {item.ArrivalTime}");
+            }
         }
     }
 }
