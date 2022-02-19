1ae7d2e93e982cd593b5b8037f99d89184cb75da
JanJanusz2000 <janjanuszpiotr@gmail.com>
2022-02-19 13:52:27 +0100

    Revert "Zatrzymanie się na problemie z wywołaniem funkcji CalculateArrivalTime"
    This reverts commit 522ef20747f0dd0b89292722d1155636ea813634.

--- taxi/Cab.cs
+++ taxi/Cab.cs
@@ -8,11 +8,14 @@ namespace taxi
 {
     public class Cab
     {
+        
+
         public Cab(int id, string car, District district)
         {
             Id = id;
             Car = car;
             CurrentDistrict = district;
+            IsAvailable = true;
         }
         public int Id { get; set; }
         public string Car { get; set; }
@@ -24,15 +27,15 @@ namespace taxi
         public District CurrentDistrict { get; set; }
         public int ArrivalTime { get; set; }
 
-        public void CalculateArrivalTime(District CurrentDisctrict)
+        public void CalculateArrivalTime(District customerDistrict)
         {
             if (IsAvailable)
             {
-                if (CurrentDisctrict.Number == CurrentDistrict.Number)
+                if (customerDistrict.Number == CurrentDistrict.Number)
                     ArrivalTime = 4;
                 else
                 {
-                    var route = Math.Abs(CurrentDisctrict.Distance - CurrentDistrict.Distance);
+                    var route = Math.Abs(customerDistrict.Distance - CurrentDistrict.Distance);
                     ArrivalTime = route * 5;
                 }
                 if (!IsAvailable)
